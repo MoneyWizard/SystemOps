7be9b72779e561c6034b8d7ecdad558af69bd216
MoneyWizard <weldongilessr@gmail.com>
2026-09-11 13:21:28 -0500

    feat: add module manifest and psm1 for package publishing

---
+++ SystemOps.psm1
@@ -0,0 +1,13 @@
+﻿# Dot-source existing function files into the module scope
+$moduleRoot = $PSScriptRoot
+. (Join-Path $moduleRoot "Get-SystemVitals.ps1")
+. (Join-Path $moduleRoot "Optimize-SystemMemory.ps1")
+. (Join-Path $moduleRoot "SystemOps.ps1")
+
+# Define module aliases
+Set-Alias -Name sysops   -Value Start-SystemOps
+Set-Alias -Name vitals   -Value Get-SystemVitals
+Set-Alias -Name reclaim  -Value Invoke-MemoryReclaim
+
+# Export public symbols
+Export-ModuleMember -Function Start-SystemOps, Get-SystemVitals, Invoke-MemoryReclaim -Alias sysops, vitals, reclaim
